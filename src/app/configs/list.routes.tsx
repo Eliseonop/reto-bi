988c238671842fb57f0488e39f1a31b4a0b99a3e
eliseodev <eliseonop@gmail.com>
2023-06-03 17:03:57 -0500

    update

--- src/app/configs/list.routes.tsx
+++ src/app/configs/list.routes.tsx
@@ -1,7 +1,6 @@
 import Home from "../main/pages/home/home";
 import Login from "../main/pages/login/Login";
 import Order from "../main/pages/orders/Order";
-import ListOrders from "../main/pages/orders/listOrders/ListOrders";
 import Register from "../main/pages/register/Register";
 import Tables from "../main/pages/tables/Tables";
 import { ProtectedRoute } from "./ProtectedRoute";
