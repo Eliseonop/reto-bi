53ddc2a91d0a02e5814b393e83d1c130267f5a16
eliseodev <eliseonop@gmail.com>
2023-06-03 17:07:47 -0500

    update

--- src/app/main/pages/register/Register.tsx
+++ src/app/main/pages/register/Register.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Container, Form, Input, Button, Error } from './register.styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { UserState } from '../../../store/slices/user/models/user.state';
-import { Navigate, redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { IRootState } from '../../../store/store';
 import { register } from '../../../store/slices/user/user.slice';
 
