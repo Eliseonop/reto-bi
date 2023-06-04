524bbc8b912723bb0845d8e2759aa893963dbe1d
eliseodev <eliseonop@gmail.com>
2023-06-03 21:24:05 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import './App.css'
 import { Provider } from 'react-redux'
 import { store } from './app/store/store'
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Main } from './app/main/Main'
-import { ProtectedRoute } from './app/configs/ProtectedRoute'
 import { MyRoutesFaq } from './app/configs/miRoutes'
 
 function App() {
